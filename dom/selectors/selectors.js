b22f952397ff69e96ca8299b7d6d7a343bcf3f9c
Susan Macchia <smacchia@endeavorrobotics.com>
2016-08-24 16:34:24 -0400

    Initial dom examples.

---
+++ dom/selectors/selectors.js
@@ -0,0 +1,14 @@
+console.log("getElementById(\"highlight\")");
+console.log(document.getElementById("hightlight"));
+console.log("getElementsByClassName(\"bolded\")");
+console.log(document.getElementsByClassName("bolded"));
+console.log("document.getElementsByTagName(\"li\")");
+console.log(document.getElementsByTagName("li"));
+console.log("document.querySelector(\"#highlight\")");
+console.log(document.querySelector("#highlight"));
+console.log("document.querySelector(\".bolded\")"); 
+console.log(document.querySelector(".bolded")); // returns only the first one
+console.log("document.querySelectorAll(\"h1\")");
+console.log(document.querySelectorAll("h1"));
+console.log("document.querySelectorAll(\".bolded\")"); 
+console.log(document.querySelectorAll(".bolded")); // returns all
