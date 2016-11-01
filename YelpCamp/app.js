8eff4021c6d7a12fa1f356f38dc8a324913af5c6
Susan Macchia <smacchia@endeavorrobotics.com>
2016-11-01 19:12:35 -0400

    V2.2
    Start adding database entries for campgrounds. Stuck at the show details right
    now where the ejs substitution is failing.

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,53 +1,84 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require('mongoose');
+
+// Will make the cat_app db if it's not there already
+mongoose.connect('mongodb://localhost:27017/yelp_camp');
 
 app.use(bodyParser.urlencoded({extended: true}));
 // This makes it so we don't have to pass "file.ejs" to
 // res.render
 app.set("view engine", "ejs");
 
-var campgrounds = [
-    {name: "Salmon Creek", image: "https://farm6.staticflickr.com/5335/14146547013_4f2a6aed8a.jpg"},
-    {name: "Granite Hill", image: "https://farm9.staticflickr.com/8422/7842069486_c61e4c6025.jpg"},
-    {name: "Mountain Goat's Rest", image: "https://farm3.staticflickr.com/2353/2069978635_2eb8b33cd4.jpg"},
-    {name: "Salmon Creek", image: "https://farm6.staticflickr.com/5335/14146547013_4f2a6aed8a.jpg"},
-    {name: "Granite Hill", image: "https://farm9.staticflickr.com/8422/7842069486_c61e4c6025.jpg"},
-    {name: "Mountain Goat's Rest", image: "https://farm3.staticflickr.com/2353/2069978635_2eb8b33cd4.jpg"},
-    {name: "Salmon Creek", image: "https://farm6.staticflickr.com/5335/14146547013_4f2a6aed8a.jpg"},
-    {name: "Granite Hill", image: "https://farm9.staticflickr.com/8422/7842069486_c61e4c6025.jpg"},
-    {name: "Mountain Goat's Rest", image: "https://farm3.staticflickr.com/2353/2069978635_2eb8b33cd4.jpg"},
-    {name: "Salmon Creek", image: "https://farm6.staticflickr.com/5335/14146547013_4f2a6aed8a.jpg"},
-    {name: "Granite Hill", image: "https://farm9.staticflickr.com/8422/7842069486_c61e4c6025.jpg"},
-    {name: "Mountain Goat's Rest", image: "https://farm3.staticflickr.com/2353/2069978635_2eb8b33cd4.jpg"}
-];
+// SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+// Schema model
+var Campground = mongoose.model("Campground", campgroundSchema);
 
 app.get("/", function(req, res) {
     res.render("landing");
 });
 
-// */new is another REST convention
+// NEW route - show form to create a new campground
 app.get("/campgrounds/new", function(req,res) {
     res.render("new.ejs")
 });
 
+// INDEX route - show list of campgrounds
 app.get("/campgrounds", function(req, res) {
     // remember, first variable is the name, second is the data from the array
-    res.render("campgrounds", {campgrounds: campgrounds});
+    // res.render("campgrounds", {campgrounds: campgrounds});
+    Campground.find({}, function(error, allCampgrounds) {
+        if (error) {
+            console.log(error);
+        } else {
+            res.render("index", {campgrounds: allCampgrounds});
+        }
+    })
 });
 
+// CREATE - add a new campground to the database
+// 
 // NOTE: the get/post have the same url, but because one is a get
 // and the other is a post, they are technically different. This is
 // the REST convention.
 app.post("/campgrounds", function(req, res) {
-    // get data from form and add to array
-    var name = req.body.name;
-    var image = req.body.image;
-    campgrounds.push({name: name, image: image});
-
-    // redirect back to campgrounds page
-    res.redirect("/campgrounds"); // default is a get request
+    // Create the campground and save to the database
+    Campground.create(
+        {
+            name: req.body.name, 
+            image: req.body.image
+        }, 
+        function(error, campground) {
+            if (error) {
+                console.log("Problem creating campground: " + error);
+            } else {
+                // redirect back to campgrounds page
+                res.redirect("/campgrounds"); // default is a get request
+            }
+        });
 });
+
+// SHOW route - must be after /new or this will ignore /new and 
+// go here. Use id because it's unique.
+app.get("/campgrounds/:id", function(req, res) {
+    Campground.findById(
+        req.params.id, 
+        function(error, found) {
+            if (error) {
+                console.log(error);
+            } else {
+                res.render("show", {which: found});
+            }
+        });
+    res.render("show");
+})
 
 app.listen(3000, function() {
     console.log("YelpCamp server started.");
